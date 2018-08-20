a2116c8f12db66dcc112ea0e3fd86898856dbd5e
arunkumardurgadevi <durgaarun.nm@gmail.com>
2018-08-20 13:57:37 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Alphabet
+{
+public static void main(String args[])
+{
+char c='a';
+if((c>='a' && c<='z')||(c>='A' && c<='Z'))
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("not a Alphabet");
+}
+}
+}
