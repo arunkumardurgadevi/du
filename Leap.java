dc26606ef86ef174df97d4b873400bdd428032fb
arunkumardurgadevi <durgaarun.nm@gmail.com>
2018-08-20 14:06:16 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Leap
+{
+public static void main(String args[])
+{
+int year=2016;
+if((year%4==0)&&(year%100!=0)&&(year%400!=0))
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
